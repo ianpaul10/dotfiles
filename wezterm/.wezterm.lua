87f02fbb8387f9960aae66d2acbbd170ae81dc5b
Ian Paul <iandouglaspaul@gmail.com>
2024-12-27 11:28:22 +0700

    wt hyperlink clicking fix

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -10,82 +10,64 @@ local config = wezterm.config_builder()
 -- config.color_scheme = "Batman"
 
 config.font = wezterm.font("JetBrains Mono")
+config.font_size = 12
 
 config.use_fancy_tab_bar = false
 
 config.colors = {
     cursor_bg = "#97cda9",
     tab_bar = {
-        -- The color of the strip that goes along the top of the window
-        -- (does not apply when fancy tab bar is in use)
         background = "#0b0022",
-
-        -- The active tab is the one that has focus in the window
         active_tab = {
-            -- The color of the background area for the tab
             bg_color = "#2b2042",
-            -- The color of the text for the tab
             fg_color = "#c0c0c0",
-
             -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
             -- label shown for this tab.
             -- The default is "Normal"
-            intensity = "Bold",
-
+            intensity = "Normal",
             -- Specify whether you want "None", "Single" or "Double" underline for
             -- label shown for this tab.
             -- The default is "None"
-            underline = "Single",
-
-            -- Specify whether you want the text to be italic (true) or not (false)
-            -- for this tab.  The default is false.
+            underline = "Double",
             italic = false,
-
-            -- Specify whether you want the text to be rendered with strikethrough (true)
-            -- or not for this tab.  The default is false.
             strikethrough = false,
         },
-
-        -- Inactive tabs are the tabs that do not have focus
         inactive_tab = {
             bg_color = "#1b1032",
             fg_color = "#808080",
-
-            -- The same options that were listed under the `active_tab` section above
-            -- can also be used for `inactive_tab`.
         },
-
-        -- You can configure some alternate styling when the mouse pointer
-        -- moves over inactive tabs
         inactive_tab_hover = {
             bg_color = "#3b3052",
             fg_color = "#909090",
             italic = true,
-
-            -- The same options that were listed under the `active_tab` section above
-            -- can also be used for `inactive_tab_hover`.
         },
-
-        -- The new tab button that let you create new tabs
         new_tab = {
             bg_color = "#1b1032",
             fg_color = "#808080",
-
-            -- The same options that were listed under the `active_tab` section above
-            -- can also be used for `new_tab`.
         },
-
-        -- You can configure some alternate styling when the mouse pointer
-        -- moves over the new tab button
         new_tab_hover = {
             bg_color = "#3b3052",
             fg_color = "#909090",
             italic = true,
-
-            -- The same options that were listed under the `active_tab` section above
-            -- can also be used for `new_tab_hover`.
         },
     },
 }
+
+-- Use the defaults as a base
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+-- make task numbers clickable
+-- the first matched regex group is captured in $1.
+table.insert(config.hyperlink_rules, {
+    regex = [[\b[tt](\d+)\b]],
+    format = "https://example.com/tasks/?t=$1",
+})
+-- make username/project paths clickable. this implies paths like the following are for github.
+-- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
+-- as long as a full url hyperlink regex exists above this it should not match a full url to
+-- github or gitlab / bitbucket (i.e. https://gitlab.com/user/project.git is still a whole clickable url)
+table.insert(config.hyperlink_rules, {
+    regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+    format = "https://www.github.com/$1/$3",
+})
 
 return config
