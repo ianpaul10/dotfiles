fa0769a0a6242d5705b4dd50508f9c1ea123bc79
Ian Paul <iandouglaspaul@gmail.com>
2025-01-20 09:21:25 +0530

    custom mini statusline

--- neovim/lua/kickstart/plugins/mini-vim.lua
+++ neovim/lua/kickstart/plugins/mini-vim.lua
@@ -1,4 +1,3 @@
----@diagnostic disable: unused-local
 local header_art_1 = [[
  ╭╮╭┬─╮╭─╮┬  ┬┬╭┬╮
  │││├┤ │ │╰┐┌╯││││
@@ -35,20 +34,44 @@ return { -- Collection of various small independent plugins/modules
     -- - sr)'  - [S]urround [R]eplace [)] [']
     require('mini.surround').setup()
 
-    -- NOTE: Simple and easy statusline.
-    -- from left to right: mode, file info icon ('+n ~n -n' for line changes),
-    -- beaker icon ('Hn In Wn En' for hints, info, warn, error counts),
-    -- circled 'L' for LSP sever count (num of + is number of servers)
-    -- file name/path
-    -- BREAK
-    -- filetype, file encoding, file size, pos, percentage
-    local statusline = require 'mini.statusline'
-    -- set use_icons to true if you have a Nerd Font
-    statusline.setup { use_icons = vim.g.have_nerd_font }
-    ---@diagnostic disable-next-line: duplicate-set-field
-    statusline.section_location = function()
-      return '%2l:%-2v|%p%%'
+    -- -- NOTE: Simple and easy statusline.
+    -- -- from left to right: mode, file info icon ('+n ~n -n' for line changes),
+    -- -- beaker icon ('Hn In Wn En' for hints, info, warn, error counts),
+    -- -- circled 'L' for LSP sever count (num of + is number of servers)
+    -- -- file name/path
+    -- -- BREAK
+    -- -- filetype, file encoding, file size, pos, percentage
+    -- local statusline = require 'mini.statusline'
+    -- -- set use_icons to true if you have a Nerd Font
+    -- statusline.setup { use_icons = vim.g.have_nerd_font }
+    -- ---@diagnostic disable-next-line: duplicate-set-field
+    -- statusline.section_location = function()
+    --   return '%2l:%-2v|%p%%'
+    -- end
+
+    local diag_signs = { ERROR = ' ', WARN = ' ', INFO = ' ', HINT = ' ' }
+    local custom_active_statusline = function()
+      local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 120 }
+      local git = MiniStatusline.section_git { trunc_width = 40 }
+      local diff = MiniStatusline.section_diff { trunc_width = 75 }
+      local diagnostics = MiniStatusline.section_diagnostics { trunc_width = 75, signs = diag_signs }
+      local lsp = MiniStatusline.section_lsp { trunc_width = 75 }
+      local filename = MiniStatusline.section_filename { trunc_width = 140 }
+      local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 120 }
+      local location = MiniStatusline.section_location { trunc_width = 75 }
+      local search = MiniStatusline.section_searchcount { trunc_width = 75 }
+
+      return MiniStatusline.combine_groups {
+        { hl = mode_hl, strings = { mode } },
+        { hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics, lsp } },
+        '%<', -- Mark general truncate point
+        { hl = 'MiniStatuslineFilename', strings = { filename } },
+        '%=', -- End left alignment
+        { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+        { hl = mode_hl, strings = { search, location } },
+      }
     end
+    require('mini.statusline').setup { use_icons = vim.g.have_nerd_font, content = { active = custom_active_statusline } }
 
     -- require('mini.tabline').setup()
     require('mini.icons').setup()
