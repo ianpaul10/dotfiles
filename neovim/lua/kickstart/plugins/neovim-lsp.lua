0905157c19ec0c0e6de7fcc2f38e7f75fdd9344d
Ian Paul <iandouglaspaul@gmail.com>
2025-02-08 17:21:59 +0700

    fix lua formatter in lsp config

--- neovim/lua/kickstart/plugins/neovim-lsp.lua
+++ neovim/lua/kickstart/plugins/neovim-lsp.lua
@@ -213,9 +213,6 @@ return { -- LSP Configuration & Plugins
     -- You can add other tools here that you want Mason to install
     -- for you, so that they are available from within Neovim.
     local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, {
-      'stylua', -- Used to format Lua code
-    })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
     require('mason-lspconfig').setup {
