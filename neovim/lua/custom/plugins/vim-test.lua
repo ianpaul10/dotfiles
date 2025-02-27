dd1733d0e0cbab07f5ad9604c698ab2dbfd56b8a
Ian Paul <iandouglaspaul@gmail.com>
2025-02-27 21:20:02 +0400

    add vimtest nvim option

--- neovim/lua/custom/plugins/vim-test.lua
+++ neovim/lua/custom/plugins/vim-test.lua
@@ -6,9 +6,11 @@ vim.keymap.set('n', '<leader>tt', ':TestNearest<CR>', { desc = '[T]est nearest' 
 vim.keymap.set('n', '<leader>tf', ':TestFile<CR>', { desc = '[T]est [f]ile' })
 vim.keymap.set('n', '<leader>ta', ':TestSuite<CR>', { desc = '[T]est [a]ll' })
 vim.keymap.set('n', '<leader>tl', ':TestLast<CR>', { desc = 'Re-run [l]ast test' })
+vim.keymap.set('n', '<leader>tn', ':TestFile -strategy=neovim<CR>', { desc = '[T]est file in [N]eoVim' })
 
 vim.cmd "let test#strategy = 'wezterm'"
 vim.cmd 'let test#wezterm#split_percent = 25'
 vim.cmd 'let test#wezterm#split_direction = "bottom"'
+vim.cmd 'let test#neovim#term_position = "horizontal 25"'
 
 return M
