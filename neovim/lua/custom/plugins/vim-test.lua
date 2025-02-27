03e18daaa3ffdf156d2f5138b0caf0539b20e6cf
Ian Paul <iandouglaspaul@gmail.com>
2025-02-27 21:19:44 +0400

    setup vimtest to use wezterm's split pane instead of inside neovim

--- neovim/lua/custom/plugins/vim-test.lua
+++ neovim/lua/custom/plugins/vim-test.lua
@@ -7,4 +7,8 @@ vim.keymap.set('n', '<leader>tf', ':TestFile<CR>', { desc = '[T]est [f]ile' })
 vim.keymap.set('n', '<leader>ta', ':TestSuite<CR>', { desc = '[T]est [a]ll' })
 vim.keymap.set('n', '<leader>tl', ':TestLast<CR>', { desc = 'Re-run [l]ast test' })
 
+vim.cmd "let test#strategy = 'wezterm'"
+vim.cmd 'let test#wezterm#split_percent = 25'
+vim.cmd 'let test#wezterm#split_direction = "bottom"'
+
 return M
