a3bab27b9fe91e296b26fc3b44d569b6a3f9374c
Ian Paul <ian.paul@shopify.com>
2025-07-10 10:01:05 -0700

    base tmux config

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
  #!/bin/bash
 
-# Colors for output
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
@@ -8,26 +7,25 @@ NC='\033[0m' # No Color
 echo -e "${GREEN}Creating necessary directories...${NC}"
 mkdir -p ~/.config/nvim
 
-# Sync zsh files
 echo -e "${GREEN}Syncing zsh files...${NC}"
 ln -sf "$PWD/zsh/.zshrc" ~/.zshrc
 ln -sf "$PWD/zsh/.zprofile" ~/.zprofile
 ln -sf "$PWD/zsh/.zshenv" ~/.zshenv
 ln -sf "$PWD/zsh/wot_in_tarnation.zsh-theme" ~/.oh-my-zsh/custom/themes
 
-# Sync vim files
 echo -e "${GREEN}Syncing vim files...${NC}"
 ln -sf "$PWD/.vimrc" ~/.vimrc
 
-# Sync neovim files
 echo -e "${GREEN}Syncing neovim configuration...${NC}"
 rm -rf ~/.config/nvim
 ln -sf "$PWD/neovim" ~/.config/nvim
 
-# Sync wezterm files
 echo -e "${GREEN}Syncing wezterm configuration...${NC}"
 rm -rf ~/.wezterm.lua
 ln -sf "$PWD/wezterm/.wezterm.lua" ~/.wezterm.lua
 
+echo -e "${GREEN}Syncing tmux configuration...${NC}"
+rm -rf ~/.tmux.conf
+ln -sf "$PWD/.tmux.conf" ~/.tmux.conf
 
 echo -e "${GREEN}Done! Configuration files have been linked.${NC}"
