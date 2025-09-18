a15c5db1edb4648ba15066606c8fb43186abfc41
Ian Paul <ian.paul@shopify.com>
2025-09-18 12:20:57 -0400

    fix vim borders

--- neovim/lua/vim-options.lua
+++ neovim/lua/vim-options.lua
@@ -78,17 +78,8 @@ vim.opt.hlsearch = true
 
 vim.opt.termguicolors = true
 
--- Add borders to floating windows. Kudos: https://vi.stackexchange.com/questions/39074/user-borders-around-lsp-floating-windows
-local _border = 'single'
-vim.lsp.handlers['textDocument/hover'] = vim.lsp.buf.hover {
-  border = _border,
-}
-vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.buf.signature_help {
-  border = _border,
-}
-vim.diagnostic.config {
-  float = { border = _border },
-}
+-- Add borders to floating windows. Kudos: https://www.reddit.com/r/neovim/comments/1kdniew/can_someone_help_how_to_add_border_to_hover
+vim.o.winborder = 'rounded'
 
 vim.opt.spelllang = 'en_us'
 vim.opt.spell = true
