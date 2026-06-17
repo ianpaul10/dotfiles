5ddb2e0de1e93c54db2275df112eef2daf5df27d
Ian Paul <ian.paul@shopify.com>
2026-06-17 08:59:25 -0700

    better command mode

--- neovim/lua/kickstart/plugins/autocompletion.lua
+++ neovim/lua/kickstart/plugins/autocompletion.lua
@@ -1,6 +1,6 @@
 return { -- Autocompletion
   'hrsh7th/nvim-cmp',
-  event = 'InsertEnter',
+  event = { 'InsertEnter', 'CmdlineEnter' },
   dependencies = {
     -- Snippet Engine & its associated nvim-cmp source
     {
